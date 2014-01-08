52d935cca6cca088e1039fcbe417a55dcd1d3f23
ta3hoon <ta3hoon@gmail.com>
2014-01-08 12:28:38 -0800

    Added decrementing size on pop, to pass test 4

--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -10,10 +10,13 @@ var makeStack = function(){
 
   // Implement the methods below
   instance.push = function(value){
-
+    size++;
   };
 
   instance.pop = function(){
+    if(size) {
+      size--;
+    }
   };
 
   instance.size = function(){
