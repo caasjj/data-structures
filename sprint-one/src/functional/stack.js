cf16f154111ffdb8ada511a396b01f277b7d4bf4
engone nze davy <engonenzedavy@yahoo.com>
2014-01-08 12:23:44 -0800

    size

--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -3,9 +3,7 @@ var makeStack = function(){
 
   // Use an object with numeric keys to store values
     var storage = {};
-  /* START PROMPT
-    var size; // Hint: set an initial value here
-  -END PROMPT */
+    var size = 0;
 
   // Implement the methods below
   instance.push = function(value){
@@ -15,6 +13,7 @@ var makeStack = function(){
   };
 
   instance.size = function(){
+    return size;
   };
   
     return instance;
