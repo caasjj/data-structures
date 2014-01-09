0475386d3f08c94c223698d2544c65014f588135
Walid Hosseini <caasjj@gmail.com>
2014-01-08 16:17:07 -0800

    Forgot to commit previously!! (oops, sorry). Implemented both queue and stack. Passes all tests.

--- sprint-one/src/functional-shared/queue.js
+++ sprint-one/src/functional-shared/queue.js
@@ -1,5 +1,37 @@
 var makeQueue = function(){
   // Hey! Copy your code from src/functional/queue.js and paste it here
+  var instance = {
+  	read: 0,
+  	write: 0,
+  	storage: {}
+  };
+ 
+  extend(instance, queueMethods);
+
+  return instance;
 };
 
-var queueMethods = {};
+var extend = function(to, from){
+  for (var key in from){
+    to[key] = from[key];
+  }
+};
+
+var queueMethods = {
+	enqueue: function(value) {
+		this.storage[this.write] = value;
+		this.write++;
+	},
+	dequeue: function(){
+    var result;
+    if(this.read < this.write) {
+      result = this.storage[this.read];
+      delete this.storage[this.read];
+      this.read++;
+    }
+    return result;
+  },
+  size: function(){
+    return this.write - this.read;
+  }
+};
--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,5 +1,33 @@
 var makeStack = function() {
-  // Hey! Copy your code from src/functional/stack.js and paste it here
+  // Hey! Copy your code from src/functional/queue.js and paste it here
+  var instance = {
+  	stackSize: 0,
+  	storage: {}
+  };
+
+  extend(instance, stackMethods);
+
+  return instance;
 };
-  
-var stackMethods = {};
+
+var stackMethods = {
+
+	push: function(value){
+		this.storage[this.stackSize] = value;
+		this.stackSize++;
+	},
+
+	pop: function(){
+		var result;
+		if(this.stackSize) {
+			this.stackSize--;
+			result = this.storage[this.stackSize];
+			delete this.storage[this.stackSize];
+		}
+		return result;
+	},
+
+	size: function(){
+		return this.stackSize;
+	}
+};
