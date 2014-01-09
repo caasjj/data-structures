aae90ba4e3b36a7708cc8c04419435c7c413f404
Hack Reactor Students <github@hackreactor.com>
2014-01-09 11:18:40 -0800

    implementation of linklist

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,12 +4,39 @@ var makeLinkedList = function(){
   list.tail = null;
 
   list.addToTail = function(value){
+    if (list.head === null) {
+      list.head = list.tail = makeNode(value);
+    } else {
+      list.tail.next =  makeNode(value);
+      list.tail = list.tail.next;
+    }
   };
 
   list.removeHead = function(){
+    list.head = list.head.next;
   };
 
   list.contains = function(target, node){
+    var temp = list.head;
+
+    while (temp.value !== target) {
+        if (temp.next === null ) {
+          return false;
+        } 
+        temp = temp.next;
+    }
+    return true;
+    // if (temp.value === target) {
+    //   return true;
+    // } 
+
+    // while( temp.next !== null )  {
+    //   temp = temp.next;
+    //   if (temp.value === target) {
+    //     return true;
+    //   }
+    // }
+    // return false;
   };
 
   return list;
