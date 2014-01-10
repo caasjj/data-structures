7276939e9dd33833b7b3c6eaf428075fc527103b
Hack Reactor Students <github@hackreactor.com>
2014-01-09 16:12:54 -0800

    implementation of the Set class

--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -7,10 +7,14 @@ var makeSet = function(){
 var setPrototype = {};
 
 setPrototype.add = function(item){
+  this._storage = this._storage || {};
+  this._storage[ ''+item ] = item;
 };
 
 setPrototype.contains = function(item){
+  return this._storage && this._storage.hasOwnProperty( '' + item );
 };
 
 setPrototype.remove = function(item){
+  delete this._storage[ '' + item ];
 };
