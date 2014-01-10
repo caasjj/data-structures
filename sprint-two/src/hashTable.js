7447977989928e524a4e462fb23ee5624b16b3d0
Hack Reactor Students <github@hackreactor.com>
2014-01-09 20:01:37 -0800

    finished hashTable

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -1,16 +1,32 @@
 var HashTable = function(){
   this._limit = 8;
   this._storage = makeLimitedArray(this._limit);
+  this._size = 0;
 };
 
 HashTable.prototype.insert = function(k, v){
-  var i = getIndexBelowMaxForKey(k, this._limit);
+  var i = getIndexBelowMaxForKey(k, this._limit);  
+  this._storage[i] = v;
+  if (++this._size >= 7 ) {
+    var temp = this._storage;
+    this._limit = 16;
+    this._storage = makeLimitedArray(this._limit);
+    extend(this._storage, temp);
+  }
 };
 
 HashTable.prototype.retrieve = function(k){
   var i = getIndexBelowMaxForKey(k, this._limit);
+  return this._storage[i] || null;
 };
 
 HashTable.prototype.remove = function(k){
+  var i = getIndexBelowMaxForKey(k, this._limit);
+  delete this._storage[i];
+  if (--this._size < 3 ) {
+    var temp = this._storage;
+    this._limit = 8;
+    this._storage = makeLimitedArray(this._limit);
+    extend(this._storage, temp);
+  }
 };
-
