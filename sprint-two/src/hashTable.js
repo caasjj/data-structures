56411b17c51bcd8f3b88df6370a3374b896a0ab2
Walid Hosseini <caasjj@gmail.com>
2014-01-11 10:53:36 -0800

    Initial implementatino of hashTable collision fix

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -9,15 +9,24 @@ HashTable.prototype.rehash = function(size) {
   var temp = this._storage;
   this._limit = size;
   this._storage = makeLimitedArray(this._limit);
-  temp.each( function(value) {
-    this._storage.insert( value.key, value);
+  temp.each( function(bin) {
+    bin.each( function(value, idx) {
+      this._storage.insert( value.key, value);
+    });
   });
 };
 
 HashTable.prototype.insert = function(k, v){
   var i = getIndexBelowMaxForKey(k, this._limit);
-  this._storage[i] = v;
-  this._storage[i].key = k;
+  if( !this._storage[i]) {
+    this._storage[i] = [];
+    this._storage[i].key = [];
+    this._storage[i] = v;
+    this._storage[i].key = k;
+  } else {
+    this._storage[i].push(v);
+    this._storage[i].key.push(k);
+  }
   if (++this._size >= Math.floor(this._limit * 0.75 ) ) {
     this.rehash(this._limit * 2);
   }
@@ -25,12 +34,14 @@ HashTable.prototype.insert = function(k, v){
 
 HashTable.prototype.retrieve = function(k){
   var i = getIndexBelowMaxForKey(k, this._limit);
-  return this._storage[i] || null;
+  var idx = this._storage[i].key.indexOf(k);
+  return this._storage[idx] || null;
 };
 
 HashTable.prototype.remove = function(k){
   var i = getIndexBelowMaxForKey(k, this._limit);
-  delete this._storage[i];
+  var idx = this._storage[i].key.indexOf(k);
+  delete this._storage[idx];
   if (--this._size < Math.floor(this._limit * 0.25) ) {
     this.rehash( this._limit / 2);
   }
